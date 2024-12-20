b9485b2ee5131b38a27216d7036890d14ce22204
aasuyadav1 <aasuy284@gmail.com>
2024-12-20 13:29:30 +0530

    done: setup

---
+++ src/components/global/generate-post.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+const GeneratePost = () => {
+  return (
+    <Button size={"lg"} className='font-semibold'>Generate Post</Button>
+  )
+}
+
+export default GeneratePost
