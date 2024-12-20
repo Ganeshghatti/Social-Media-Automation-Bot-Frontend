c04de372140051d1a9bb7b84c0a1b09cdf778417
aasuyadav1 <aasuy284@gmail.com>
2024-12-20 13:40:27 +0530

    add: theme changer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import GeneratePost from "@/components/global/generate-post";
 import TwitterCard from "@/components/global/card";
+import ThemeChange from "@/components/global/theme-change";
 
 export default function Home() {
   return (
     <main className="p-8">
       <div className="space-y-6 mt-8">
+        <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">
           Upload Twitter Post by Automation
         </h1>
+        <ThemeChange/>
+        </div>
         <GeneratePost />
       </div>
       <div className="mt-20 grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 gap-x-10 gap-y-4">
