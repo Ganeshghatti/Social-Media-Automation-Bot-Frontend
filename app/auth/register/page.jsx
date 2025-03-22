4fcb41321be495d123880cff6c25fd55bef4e1a9
Adarshtz <adarshtz@outlook.com>
2025-03-22 18:02:12 +0530

    changes to register form component and resgiter page

--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -2,6 +2,7 @@
 
 import { RegisterForm } from "@components/register-form";
 import useAuthToken from "@hooks/useAuthToken";
+import { BotIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -15,6 +16,22 @@ const Page = () => {
   return (
     <div className="grid min-h-svh bg-navBg lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a
+            href="#"
+            className="flex items-center gap-2 font-medium text-white"
+          >
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <Image
+                src={"/logo.jpg"}
+                width={24}
+                height={24}
+                alt="squirrel logo"
+              />
+            </div>
+            Squirrel Bot.
+          </a>
+        </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
             <RegisterForm />
